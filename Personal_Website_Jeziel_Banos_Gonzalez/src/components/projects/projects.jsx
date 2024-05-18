90f3e828170309ca1ad4c34f32f1dc284b70ac8b
Jeziel-BG <Jezielgon@hotmail.com>
2024-05-18 01:34:24 -0700

    Update projects.jsx

--- Personal_Website_Jeziel_Banos_Gonzalez/src/components/projects/projects.jsx
+++ Personal_Website_Jeziel_Banos_Gonzalez/src/components/projects/projects.jsx
@@ -16,7 +16,7 @@ export function CreateProjects() {
         <p className="project-description">
           A collection of Machine Learning models trained on the same dataset
           with different training approaches that can accurately predict if a
-          given text is sexist. (Group Project)
+          given twitter post is considered sexist. (Group Project)
         </p>
         <button className="project-button" onClick={() => window.open('/documents/request.pdf')}>View</button>
       </div>
@@ -43,6 +43,19 @@ export function CreateProjects() {
       <p className="project-description">
         A portfolio website made using React that introduces who I am and the 
         projects that I've worked on.
+      </p>
+      <button className="project-button" onClick={() => window.open('https://jeziel-banos-gonzalez.netlify.app/', "_self")}>View</button>
+      </div>
+
+      <div className="project-folder"> 
+      <div className="icon">
+        <img className="icon-image" src="/images/java-logo.png"></img>
+      </div>
+      <h1 className="project-title">Mock Arcade Database Design & Implementation</h1>
+      <p className="project-description">
+        A collaboratively built java program utilizing JDBC to connect to an Oracle SQL Database 
+        for a restaurant arcade buisness allowing front desk
+        workers to update and query the database. 
       </p>
       <button className="project-button" onClick={() => window.open('https://jeziel-banos-gonzalez.netlify.app/', "_self")}>View</button>
       </div>
